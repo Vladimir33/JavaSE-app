7f8e4fab0a790a97794229dcbe6c2281bd3bfdea
Vladimir33 <vod.vladimir33@gmail.com>
2016-06-04 23:52:12 +0300

    minor correction

--- src/com/MainArray.java
+++ src/com/MainArray.java
@@ -1,82 +1,55 @@
 package com;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
 import com.urise.webapp.storage.SortedArrayStorage;
 import com.urise.webapp.storage.Storage;
 
 /**
  * Test for com.urise.webapp.storage.ArrayStorage
  */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class MainArray {
+
     private final static Storage ARRAY_STORAGE = new SortedArrayStorage();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
-        while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | update uuid | clear | exit): ");
-            String[] params = reader.readLine().trim().toLowerCase().split(" ");
-            if (params.length < 1 || params.length > 2) {
-                System.out.println("Неверная команда.");
-                continue;
-            }
-            if (params.length == 2) {
-                params[1] = params[1].intern();
-            }
-            switch (params[0]) {
-                case "list":
-                    printAll();
-                    break;
-                case "size":
-                    System.out.println(ARRAY_STORAGE.size());
-                    break;
-                case "save":
-                    r = new Resume();
-                    r.setUuid(params[1]);
-                    ARRAY_STORAGE.save(r);
-                    printAll();
-                    break;
-                case "update":
-                    r = new Resume();
-                    r.setUuid(params[1]);
-                    ARRAY_STORAGE.update(r);
-                    printAll();
-                    break;
-                case "delete":
-                    ARRAY_STORAGE.delete(params[1]);
-                    printAll();
-                    break;
-                case "get":
-                    System.out.println(ARRAY_STORAGE.get(params[1]));
-                    break;
-                case "clear":
-                    ARRAY_STORAGE.clear();
-                    printAll();
-                    break;
-                case "exit":
-                    return;
-                default:
-                    System.out.println("Неверная команда.");
-                    break;
-            }
-        }
+    public static void main(String[] args) {
+        final Resume r1 = new Resume();
+        r1.setUuid("uuid1");
+        final Resume r2 = new Resume();
+        r2.setUuid("uuid2");
+        final Resume r3 = new Resume();
+        r3.setUuid("uuid3");
+        final Resume r4 = new Resume();
+        r4.setUuid("uuid4");
+        final Resume r5 = new Resume();
+        r5.setUuid("uuid5");
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r5);
+        ARRAY_STORAGE.save(r4);
+
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
+     //   ARRAY_STORAGE.delete(r2.getUuid());
+     //   ARRAY_STORAGE.delete(r3.getUuid());
+
+
+        printAll();
+        ARRAY_STORAGE.clear();
+        printAll();
+
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
-        Resume[] all = ARRAY_STORAGE.getAll();
-        System.out.println("----------------------------");
-        if (all.length == 0) {
-            System.out.println("Empty");
-        } else {
-            for (Resume r : all) {
-                System.out.println(r);
-            }
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
         }
-        System.out.println("----------------------------");
     }
 }
