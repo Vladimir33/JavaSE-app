1006591eee5f7ea872923de9cc34bc67967baa57
Vladimir33 <vod.vladimir33@gmail.com>
2016-06-15 17:39:03 +0300

    fix TODO in MainReflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -4,12 +4,14 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Created by Vladimir on 13.06.2016.
  */
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -17,6 +19,8 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection
+        Method method = r.getClass().getMethod("toString");
+        System.out.println((String) method.invoke(r));
         System.out.println(r);
     }
 }
